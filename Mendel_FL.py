03828fea299e1c53eac82564da00a6260f9ab6a7
CHC <jr55662003@users.noreply.github.com>
2016-04-16 16:10:09 +0800

    Mendel's First Law
    ID: IPRB

---
+++ Mendel_FL.py
@@ -0,0 +1,39 @@
+import random
+	
+def MFL(k, m, n):   
+	# "YY" * k, "Yy" * m, "yy" * n
+	# Mathematical model 
+	denon = (k + m + n) * (k + m + n - 1)
+	YYxYY = (k * (k - 1)) / denon
+	YYxYy = 2 * ((k * m) / denon)
+	YYxyy = 2 * k * n / denon
+	YyxYy = 0.75 * m * (m - 1) / denon
+	Yyxyy = m * n / denon
+	return YYxYY + YYxYy + YYxyy + YyxYy + Yyxyy
+
+def Population(k, m, n):
+	#Simulation model
+	P = ["YY"] * k + ["Yy"] * m + ["yy"] * n       #create a list of organisms
+	times = 1000000
+	d = 0
+	for x in range(0, times):
+		A = random.randint(0, k + m + n - 1)
+		B = random.randint(0, k + m + n - 1)
+		if Mating(P[A], P[B]):
+			d = d + 1
+	return d / times
+
+def Mating(A, B):    #return True if child is dominant
+	if A == "YY" or B == "YY":
+		return True
+	elif A == "Yy" and B == "Yy":
+		if random.randint(1, 4) % 4 != 0:
+			return True
+		else:
+			return False
+	elif A == "yy" and B == "yy":
+		return False 
+	else:          #Yyxyy
+		return random.randint(0, 1)
+
+print(MFL(19, 15, 21))
