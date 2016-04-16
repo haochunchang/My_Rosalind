a21c4ab845a5fa2a9f3597a8799d1861c5f6e4f3
CHC <jr55662003@users.noreply.github.com>
2016-04-16 16:23:41 +0800

    Mendel's First Law

--- IPRB.py
+++ IPRB.py
@@ -1,3 +1,10 @@
+'''
+Given: Three positive integers kk, mm, and nn, representing a population containing k+m+nk+m+n organisms: 
+kk individuals are homozygous dominant for a factor, mm are heterozygous, and nn are homozygous recessive.
+
+Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele 
+(and thus displaying the dominant phenotype). Assume that any two organisms can mate.
+'''
 import random
 	
 def MFL(k, m, n):   
