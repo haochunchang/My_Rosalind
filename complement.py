3d05828102667e2ce2e1d7695b34f54f13c49aed
CHC <jr55662003@users.noreply.github.com>
2016-04-16 16:07:40 +0800

    Complementing and Reversing a Strand of DNA
    ID:REVC

---
+++ complement.py
@@ -0,0 +1,31 @@
+def validate_base(base, RNAflag = False):
+	RNAbase = set("AUCG")
+	DNAbase = set("ATCG")
+	return base <= RNAbase if RNAflag else DNAbase
+
+def compliment(base):
+	RNA = "AUCG"
+	DNA = "ATCG"
+	new_base = ""
+	if validate_base(base):
+		for b in base:
+			if b == "A":
+				new_base += "T"
+			elif b == "T":
+				new_base += "A"
+			elif b == "C":
+				new_base += "G"
+			elif b == "G":
+				new_base += "C"
+	base = new_base
+	return base
+
+def reverse(base):
+	new_base = ""
+	for i in base[::-1]:
+		new_base += i
+	base = new_base
+	return base
+
+firstbase = "AAAACCCGGT"
+print(reverse(compliment(firstbase)))
