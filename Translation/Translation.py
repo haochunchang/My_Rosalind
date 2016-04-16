48fa5bc97857a71d126f1f87eed9625c23a12ff6
CHC <jr55662003@users.noreply.github.com>
2016-04-16 16:15:47 +0800

    Translating RNA into Protein
    ID: PROT

---
+++ Translation/Translation.py
@@ -0,0 +1,24 @@
+def Translate(codons):
+	codons = [codons[i:i + 3] for i in range(0, len(codons), 3)]     #separate each codon
+	table = {}
+	protein = ""
+
+	with open("CodonTable.txt") as t:                             #create a table from CodonTable.txt
+		for line in t.read().split(" "):
+			if line[0:3].isalpha():
+				table[line[0:3]] = line[3:]	
+			else:
+				table[line[1:4]] = line[4:]
+
+	for codon in codons:
+		if table[codon] == "Stop" :
+			break
+		else:
+			protein = protein + table[codon]
+
+	return protein	
+
+with open("protein.txt", "w") as p:
+	i = open(input("data input: ")) 
+	p.write(Translate(i.read()))
+	i.close()
