1b6dc93b603b31d06b05b401a8f59e7e6e253af1
CHC <jr55662003@users.noreply.github.com>
2016-04-16 16:12:16 +0800

    Finding a Motif in DNA
    ID: SUBS

---
+++ Motif.py
@@ -0,0 +1,18 @@
+def get_index(s, char):
+	return [i[0] for i in enumerate(s) if i[1] == char]     #return a list of index of char in s
+
+def Motif(s, subs):
+	position = []
+	index = get_index(s, subs[0])
+	for i in range(0, len(index)):
+		if subs == s[index[i]:index[i] + len(subs)]:
+			position.append(index[i] + 1)
+
+	return position
+
+with open(input("file input: ")) as f:
+	s = f.readline()[:-1]
+	subs = f.readline()[:-1]
+	m = open("motif.txt", "w")
+	m.write(str(Motif(s, subs)))
+	m.close()
