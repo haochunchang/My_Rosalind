dcf001464329b2680e5fd2a53180a63b63158653
jr55662003 <jr55662013@gmail.com>
2016-04-22 18:48:38 +0800

    Translating RNA into Protein

--- Translation/PROT.py
+++ Translation/PROT.py
@@ -3,18 +3,11 @@ Given: An RNA string ss corresponding to a strand of mRNA (of length at most 10 
 
 Return: The protein string encoded by ss.
 '''
-
+#Translate RNA string into Protein string
 def Translate(codons):
 	codons = [codons[i:i + 3] for i in range(0, len(codons), 3)]     #separate each codon
-	table = {}
+	table = Build_Table()
 	protein = ""
-
-	with open("CodonTable.txt") as t:                             #create a table from CodonTable.txt
-		for line in t.read().split(" "):
-			if line[0:3].isalpha():
-				table[line[0:3]] = line[3:]	
-			else:
-				table[line[1:4]] = line[4:]
 
 	for codon in codons:
 		if table[codon] == "Stop" :
@@ -24,7 +17,19 @@ def Translate(codons):
 
 	return protein	
 
-with open("protein.txt", "w") as p:
-	i = open(input("data input: ")) 
-	p.write(Translate(i.read()))
-	i.close()
+#create a dictionary of condon table from CodonTable.txt
+def Build_Table():
+	table = {}
+	with open("CodonTable.txt") as t:                             
+		for line in t.read().split(" "):
+			if line[0:3].isalpha():
+				table[line[0:3]] = line[3:]	
+			else:
+				table[line[1:4]] = line[4:]
+	return table
+
+if __name__ == '__main__':
+	with open("protein.txt", "w") as p:
+		i = open(input("data input: ")) 
+		p.write(Translate(i.read()))
+		i.close()
