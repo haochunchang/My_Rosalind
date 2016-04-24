208c63915be0f7248680f442429e4759a1b56dd1
jr55662003 <jr55662013@gmail.com>
2016-04-24 09:03:17 +0800

    add compliement & reverse function and update table of content

--- DataProcess.py
+++ DataProcess.py
@@ -1,6 +1,11 @@
 '''
 	It is a library of methods of data processing
 
+Table of contents:
+1. ReadFASTA
+2. validate_base
+3. compliment_DNAbase
+4. reverse_base
 '''
 # 1.process FASTA into a list of seq.
 def ReadFASTA(file):
@@ -26,6 +31,30 @@ def validate_base(base, rnaflag = False):
 		return False
 	else:
 		return True
+
+# 3. compliment the DNA nucleotide base
+def compliment_DNAbase(base):
+	new_base = ""
+	if validate_base(base.upper()):
+		for b in base:
+			if b == "A":
+				new_base += "T"
+			elif b == "T":
+				new_base += "A"
+			elif b == "C":
+				new_base += "G"
+			elif b == "G":
+				new_base += "C"
+	return new_base
+
+# 4. reverse the nucleotide base
+def reverse_base(base):
+	new_base = ""
+	for i in base[::-1]:
+		new_base += i
+	return new_base
+
+
 
 
 
